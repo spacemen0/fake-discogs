787551801b4946a930f74a32fd3ec30de8543892
WalkingWithSpacemen3 <frakctures@outlook.com>
2023-11-02 13:11:31 +0100

    functioning

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,32 +1,34 @@
-import React, { useCallback, useEffect } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import TabMenu from "./TabMenu"
 import Filter from "./Filter"
 import RecordCard from "./RecordCard"
 
-function FetchRecords() {
-    const fetchRecords = useCallback(async () => {
-        const response = await fetch("http://localhost:1111/get-records")
+function Container() {
+    const [records, setRecords] = useState([])
+
+    async function fetchRecords() {
+        const response = await fetch("http://localhost:1111/api/v1/get-records", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: "[]"
+        })
         const records = await response.json()
-        console.log(records)
-    }, [])
+        setRecords(records)
+    }
 
     useEffect(() => {
         fetchRecords()
-    }, [fetchRecords])
-}
+    }, [])
 
-function Container() {
-
-    FetchRecords()
-    useEffect(() => {
-        document.title = "Record Store"
-    }
-    , [])
     return (
         <div className="container">
             <TabMenu/>
             <Filter genres={["rock","jazz","pop"]} years={["1001","1002"]} statuses={["sold","selling"]} />
-            <RecordCard title={"title"} artist={"artist"} genre={"rock"} year={"2002"}/>
+            {records.map(record => (
+                <RecordCard key={record.id} title={record.title} artist={record.artist} genre={record.genre} year={record.year} description={record.description}/>
+            ))}
         </div>
     )
 }
--- src/components/container/RecordCard.jsx
+++ src/components/container/RecordCard.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react';
 
-const RecordCard = ({ title, artist, year, genre }) => {
+const RecordCard = ({ title, artist, year, genre , description}) => {
   return (
     <div className="record-card">
       <h2>{title}</h2>
       <p>{artist}</p>
       <p>{year}</p>
       <p>{genre}</p>
+      <p>{description}</p>
     </div>
   );
 };

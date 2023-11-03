81674ef0b0c04676d335af70190002177f6ac5ba
WalkingWithSpacemen3 <frakctures@outlook.com>
2023-11-03 21:18:06 +0100

    Refactor search functionality in SearchBar component.

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,11 +1,38 @@
-import React from "react";
-
+import { useState } from "react";
+import { useRecordsContext } from "../../contexts/RecordsContext";
 const SearchBar = () => {
-  const { updateRecords } = useAppContext();
+  const { updateRecords } = useRecordsContext();
+  const [searchTerm, setSearchTerm] = useState("");
+  async function searchRecords() {
+    const response = await fetch(
+      "http://localhost:1111/api/v1/search-records?search_term=" + searchTerm,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: "[]",
+      }
+    );
+    const records = await response.json();
+    updateRecords(records);
+  }
   return (
     <div>
-      <input type="text" placeholder="Search..." />
-      <button>Search</button>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          searchRecords();
+        }}
+      >
+        Search
+      </button>
     </div>
   );
 };

438550e7074f7a9e4f8da4d7dc12a5ad00dab370
WalkingWithSpacemen3 <frakctures@outlook.com>
2023-11-11 21:42:09 +0100

    complete user page

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,12 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import config from "../config";
 import UserRecords from "../components/container/UserRecords";
 import Header from "../components/header/Header";
+import UserCard from "../components/container/UserCard";
 function User() {
   const { username } = useParams();
-  window.alert(username);
+  const [user, setUser] = useState({});
+  async function searchUser() {
+    const response = await fetch(
+      `${config.apiUrl}get-user-by-username/${username}`,
+      {
+        method: "GET",
+      }
+    );
+    const user = await response.json();
+    setUser(user);
+  }
+  useEffect(() => {
+    searchUser();
+  }, [username]);
   return (
     <div>
       <Header />
+      <UserCard user={user} />
+      <h1>{username}'s Records</h1>
       <UserRecords username={username} />
     </div>
   );
